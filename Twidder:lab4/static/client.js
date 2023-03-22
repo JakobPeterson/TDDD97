3661013f5440e4361f2432b9b1f5a4d3e8d49db7
JakobPeterson <125878104+JakobPeterson@users.noreply.github.com>
2023-03-22 13:18:36 +0100

    Add files via upload

--- Twidder:lab4/static/client.js
+++ Twidder:lab4/static/client.js
@@ -18,7 +18,7 @@ displayView = function(){
 
 
 function socket_connect(){ //socket handler
-    var socket = new WebSocket('http://twiddertwidder.herokuapp.com/profileview');
+    var socket = new WebSocket('ws://twiddertwidder.herokuapp.com/profileview');
     socket.onopen = function(){
         socket.send(localStorage.getItem('token'));
     }
